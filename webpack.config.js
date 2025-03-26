d8baeabfcd7bfe047c7f8b891d6f0061193ed713
BillMaN89 <mpouloukos-synas@hotmail.com>
2025-03-26 13:26:56 +0200

    Initial file creation, dependencies installation

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, 
+  },
+  mode: "development", 
+  devtool: "eval-source-map", 
+  devServer: {
+    static: "./dist", // Δείχνει στον browser πού να σερβίρει αρχεία
+    open: true,       // Ανοίγει αυτόματα τον browser
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+        {
+          test: /\.(png|jpg|jpeg|gif|svg)$/i, // Υποστήριξη για εικόνες
+          type: "asset/resource", // Μετατρέπει τις εικόνες σε αρχεία στον dist  
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+  },
+};
