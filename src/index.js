694e11030e4e6699e51821c9f1a189a277b510ec
BillMaN89 <mpouloukos-synas@hotmail.com>
2025-03-27 13:25:06 +0200

    update Project class, function test code on index

--- src/index.js
+++ src/index.js
@@ -2,21 +2,54 @@ import Task from "./modules/task.js";
 import Project from "./modules/project.js";
 
 //test code
+const myProject = new Project("Daily Tasks");
+
 const task1 = new Task(
     "Buy groceries",
     "Milk, Eggs, Bread",
     "2025-04-01",
     "medium"
 );
-  
-const myProject = new Project("Daily Tasks");
+
+const task2 = new Task(
+    "Go to the gym",
+    "It's time for personal",
+    "2025-03-27",
+    "high"
+);
+
+const task3 = new Task(
+    "Do the dishes",
+    "you know you want to",
+    "2025-03-26",
+    "low"
+);
+
+const task4 = new Task(
+    "Do the laundry",
+    "it's been 2 days",
+    "2025-03-28",
+    "medium"
+)
+
 const result = myProject.addTask(task1);
 console.log(result); // Πρέπει να δεις "Task 'Buy groceries' added successfully!"
-
+myProject.addTask(task2);
+myProject.addTask(task3);
+myProject.addTask(task4);
 console.log("All tasks in project:");
 console.table(myProject.tasksList);
 
-const result2 = myProject.addTask("go to gym"); // string, όχι Task object
-console.log(result2); // Πρέπει να δεις το warning + "Invalid Task, please try again"
+task2.toggleComplete();
+task4.toggleComplete();
 
+console.log("Completed Tasks:");
+console.table(myProject.getCompletedTasks());
+
+console.log("Pending Tasks: ");
+console.table(myProject.getPendingTasks());
+
+const removal = myProject.clearCompletedTasks();
+console.log(removal);
+console.table(myProject.tasksList);
 
--- src/modules/project.js
+++ src/modules/project.js
@@ -19,6 +19,29 @@ class Project {
     get tasksList(){
         return this.tasks;
     }
+
+    removeTask(index){
+        if ((index >= 0) && (index < this.tasks.length)){
+            this.tasks.splice(index,1);
+            return "Task removed successfully!";
+        }else {
+            console.warn("Task not found on this index");
+            return "Invalid index, task not removed!";
+        }
+    }
+
+    getCompletedTasks() {
+        return this.tasks.filter((task) => task.completed);
+    }
+
+    getPendingTasks() {
+        return this.tasks.filter((task) => !task.completed);
+    }
+
+    clearCompletedTasks() {
+        this.tasks = this.tasks.filter((task) => !task.completed);
+        return "Completed Tasks removed!"
+    }
 }
 
 export default Project;
